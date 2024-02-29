8b04c3d43a6ce06ee4922e37dff14326fd9614aa
mac <mac@macs-MacBook-Air.local>
2024-02-29 13:11:35 +0100

    This shows my days of little beginings

---
+++ automation_basics.py
@@ -0,0 +1,16 @@
+#Conditional_statements
+if failed_attempts > 5
+    print("Account locked")
+# > < >= <= == !=
+if operating_system == "OS 2"
+    print("Updates needed")
+operating_system == "OS 2"
+else:
+    print("No updates needed")
+
+#Loops - for, while.
+for i in [1,2,3,4]:
+    print(i)
+    range (0,10)
+for i in range(10):
+    print ("Cannot connect to the destination")   
---
+++ fbi_test.py
@@ -0,0 +1,2 @@
+# fbi_starts_writing_python_officialy_261023
+print "Welcome to python programing FBI"
